9dbb206b784ac95374e219758ae010e7ebce8a7b
Iliana Dulina <ilyana.dulina07@gmail.com>
2025-07-11 12:43:54 +0300

    design(App): finished ui according to coursera criteria

--- src/Hero/Hero.js
+++ src/Hero/Hero.js
@@ -8,8 +8,10 @@ function HeroComponent() {
             <div className="hero_text">
                 <h1>Little Lemon</h1>
                 <h2>Chicago</h2>
-                <p>Welcome to Little Lemon, a charming restaurant in Chicago.</p>
-                <Button type="primary">Reserve a Table</Button>
+                <p>Welcome to Little Lemon, a cozy Mediterranean restaurant nestled in the heart of Chicago.</p>
+                <p>We bring the vibrant flavors of the Mediterranean coast to your plate with fresh ingredients, bold spices, and timeless recipes.</p>
+                <p>Whether you're stopping by for a quick lunch or a relaxing dinner, Little Lemon offers a warm, inviting atmosphere and dishes that feel both comforting and exciting. Come taste the sunshine of the Mediterranean—right here in Chicago.</p>
+                <Button className='hero_button' type="primary">Reserve a Table</Button>
             </div>
             <div className="hero_image">
                 <img src="/Hero_Dish.jpg" alt="Hero Dish" />
