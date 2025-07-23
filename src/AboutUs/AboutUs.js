36098b294209f16cf059688188fbca10b1bd1c86
Iliana Dulina <ilyana.dulina07@gmail.com>
2025-07-23 12:59:37 +0300

    refactor(App): changed all flex to antd grid

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Layout, Menu, Button, Card, Row, Col } from 'antd';
+import { Layout, Row, Col } from 'antd';
 import './AboutUs.css';
 
 const { Content } = Layout;
 
 function AboutUsComponent() {
     return (
-        <section className='about-us'>
-          <div className='about-us_text'>
-            <h2>About Us</h2>
-            <p>Little Lemon is a family-owned Mediterranean restaurant located in the heart of Chicago.</p>
-            <p>We are committed to providing our customers with the best dining experience, using only the freshest ingredients and traditional recipes.</p>
-            <p>Our team is passionate about food and hospitality, ensuring that every visit is memorable.</p>
-            <p>Join us for a meal and experience the warmth of our family atmosphere.</p>
-          </div>
-          <div className='about-us_image'>
-            <img src="/Owners.webp" alt="Owners"/>
-          </div>
-        </section>
+        <Content>
+            <section className='about-us'>
+                <Row gutter={[24, 24]} align='middle'>
+                    <Col xs={24} md={12} className='about-us_text'>
+                        <h2>About Us</h2>
+                        <p>Little Lemon is a family-owned Mediterranean restaurant located in the heart of Chicago.</p>
+                        <p>We are committed to providing our customers with the best dining experience, using only the freshest ingredients and traditional recipes.</p>
+                        <p>Our team is passionate about food and hospitality, ensuring that every visit is memorable.</p>
+                        <p>Join us for a meal and experience the warmth of our family atmosphere.</p>
+                    </Col>
+                    <Col xs={24} md={12} lg={12} className='about-us_image'>
+                        <img src='/Owners.webp' alt='Owners' />
+                    </Col>
+                </Row>
+            </section>
+        </Content>
     );
 }
 
